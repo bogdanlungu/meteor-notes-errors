a6af4523e09ef83f7dec2c4986e04ce8f697301f
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2015-11-12 22:42:47 +0200

    Started the testing for the package

--- meteor-notes-errors-tests.js
+++ meteor-notes-errors-tests.js
@@ -1,3 +1,13 @@
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+Tinytest.add("Errors", function(test) {
+  test.equal(Errors.errors.find({}).count(), 0);
+  Errors.throwError('A new error!');
+  test.equal(Errors.errors.find({}).count(), 1);
+  Errors.errors.remove({});
+});
+
+Tinytest.add("Notifications", function(test) {
+  test.equal(Errors.notifications.find({}).count(), 0);
+  Errors.throwNotification('A new notification!');
+  test.equal(Errors.notifications.find({}).count(), 1);
+  Errors.notifications.remove({});
 });
